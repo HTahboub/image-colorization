50297dd573ceb87e1be1441588fe0fb7c6217986
astelmach01 <astelmach20@gmail.com>
2024-04-08 21:24:57 -0400

    starting fusion

---
+++ ddcolor/models/fusion.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+
+class FusionModule(nn.Module):
+    
+    def __init__(hyperparams):
+        pass
+
+    def forward(image_embedding: torch.Tensor, color_embedding: torch.Tensor) -> torch.Tensor:
+        pass
+        
