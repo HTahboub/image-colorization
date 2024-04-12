b11a864037525d7a224895cebdfc369f3a0d32a3
Hamza Tahboub <hamza.tahboub8@gmail.com>
2024-04-12 13:03:51 -0400

    Fixed dimensions 224 -> 256

--- ddcolor/models/fusion.py
+++ ddcolor/models/fusion.py
@@ -18,7 +18,8 @@ class FusionModule(nn.Module):
         Returns:
             A tensor of shape (B, 2, H, W).
         """
-        assert image_embedding.shape[0] == color_embedding.shape[1]
+        assert image_embedding.shape[0] == color_embedding.shape[0]
+        assert image_embedding.shape[1] == color_embedding.shape[2]
 
         f_hat = torch.einsum("bkc,bchw -> bkhw", color_embedding, image_embedding)
         y_hat = nn.Conv2d(
@@ -29,24 +30,16 @@ class FusionModule(nn.Module):
 
 if __name__ == "__main__":
     c = 512
-    h = 64
-    w = 100
+    h = 256
+    w = 256
     k = 128
 
     # Create an instance of the fusion module
     fusion_module = FusionModule()
 
     # Test batched input
-    batch_size = 4
-    image_embedding_batch = torch.randn(
-        batch_size, c, h, w
-    )  # Example shape: (B, C, H, W)
-    color_embedding_batch = torch.randn(batch_size, k, c)  # Example shape: (B, K, C)
-
+    b = 4
+    image_embedding_batch = torch.randn(b, c, h, w)
+    color_embedding_batch = torch.randn(b, k, c)
     output_batch = fusion_module(image_embedding_batch, color_embedding_batch)
-
-    expected_batch_shape = (batch_size, 2, h, w)
-    assert (
-        output_batch.shape == expected_batch_shape
-    ), f"Output shape {output_batch.shape} does not match the expected shape {expected_batch_shape}"
-    print("Batched input test passed!")
+    assert output_batch.shape == (b, 2, h, w)
